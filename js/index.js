c7ef55d4af53ad654e0d6b0462d1827543a88e99
Allan ROLLE <allan.rolle@laposte.net>
2019-03-06 20:45:49 +0100

    MC-Fix visual bug regarding ad in top header (section intro) and add js script to remove ad

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 (function(){
 
+  // Display Menu
+
   var menuButton = document.querySelector('.js-menu__responsive__tile');
   var menu = document.querySelector('.js-menu__links');
 
@@ -9,5 +11,17 @@
 
   menuButton.addEventListener('click', displayMenu);
 
+
+
+  // Hide add at the top of the page
+
+  var closeAdButton = document.querySelector('.js-login__button');
+  var ad = document.querySelector('.js-login__ad');
+
+  function hideAd(){
+    ad.style.display = "none";
+  }
+
+  closeAdButton.addEventListener('click', hideAd);
 
 })()
